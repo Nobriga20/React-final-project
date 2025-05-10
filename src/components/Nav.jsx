390fe0d8c95a6cb77a2f125e8dfd7c28b5fdef71
Nobriga20 <courtvizn@gmail.com>
2025-05-10 10:53:44 -0700

    first push

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Nav () {
+    return (<nav>
+       
+    <ul class="nav__bar">
+    
+    <li><a href="Home" class="nav__link">Home</a></li>
+    
+    <li>
+    
+    <a href="Find Your Movie" class="nav__link">Find Your Movie</a>
+    
+    </li>
+    
+    <li><a href="Contact" class="nav__link">Contact</a></li>
+    
+    </ul>
+    
+    </nav>
+    )
+}
+
+export default Nav

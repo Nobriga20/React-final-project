20651db866d3bd53cfc5ea6a5b79472340d18070
Nobriga20 <courtvizn@gmail.com>
2025-05-24 15:02:53 -0700

    next push

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 import Landing from "./components/Landing"
 import Movies from "./components/Movies"
 import Header from "./components/Header"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 function Movies() {
   const [data, setData] = useState(null);
   const [input, setInput] = useState("")
   
-  const getData = async () => {
+const getData = async () => {
     const response = await fetch(`http://www.omdbapi.com/?s=${input}&apikey=dd4916f0`);
     const result = await response.json();
     console.log (result)
@@ -20,17 +21,33 @@ function Movies() {
         <div className="container">
           <div className="wrapper"></div>
           <div>
-            <input type="text" Title={setInput} />
+            <input 
+              type="text" 
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
           </div>
-          <button onclick={getData}>
+          <button onClick={getData}>
             search
           </button>
            
-          <div className="pagination"></div>
+           {data && data.Search && (
+            <div>
+              {data.Search.map((movie, index) => (
+                <div key={index}>
+                  <Link to={`/movie/${movie.imdbID}`}>
+                    <h3>{movie.Title}</h3>
+                  </Link>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </section>
   );
 }
+  
 
 export default Movies;
